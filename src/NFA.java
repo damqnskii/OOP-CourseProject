a2a8b041a07c4664fd7bcdef49b4555a58365a99
damqnskii <damqnskiqq@gmail.com>
2025-04-04 01:18:40 +0300

    asd

---
+++ src/NFA.java
@@ -0,0 +1,9 @@
+import java.util.Set;
+
+public class NFA {
+    Set<State> states;
+    State startState;
+    Set<Character> alphabet;
+
+
+}
--- src/State.java
+++ src/State.java
@@ -1,3 +1,19 @@
-public class State {
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class State {
+    private String name;
+    boolean isFinal;
+    Map<Character, Set<State>> transitions;
+    Set<State> epsilonTransitions;
+
+    public void addTransition(char symbol, State nextState) {
+        transitions.putIfAbsent(symbol, new HashSet<>());
+        transitions.get(symbol).add(nextState);
+    }
+
+    public void addEpsilonTransition(State nextState) {
+        epsilonTransitions.add(nextState);
+    }
 }
